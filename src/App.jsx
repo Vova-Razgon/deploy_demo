c4946b39c006ed81ff16aeb4d80fa98d0c4fd706
Vova Razgon <vrazgon@live.com>
2023-06-20 15:53:38 +0300

    add react app

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+function Dates() {
+  const dateIs = new Date().toLocaleDateString();
+  return dateIs;
+}
+
+function App() {
+  const timeNow = new Date().toLocaleTimeString();
+  const vovaStyle = {
+    backgroundColor: 'black',
+    color: 'red'
+  }
+
+  return <main>
+    <header>
+      <h1>
+        vova's website
+      </h1>
+    </header>
+    <section>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Nihil vero laboriosam facere ipsa itaque
+        laborum corporis, error quibusdam? Dignissimos consequuntur
+        sed fugit eaque pariatur sit aliquid maiores
+        illum quasi dolores!
+      </p>
+    </section>
+    <div style={vovaStyle} id="time-element">
+      <p>the time is {timeNow}</p>
+      <Dates/>
+    </div>
+    <footer>all rights reserved</footer>
+  </main>
+}
+
+export default App
---
+++ src/assets/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkjRkgQAdcqwHnBHk8Pj_C_hadIl4mkFg",
+  authDomain: "web-deploy-demo-2bede.firebaseapp.com",
+  projectId: "web-deploy-demo-2bede",
+  storageBucket: "web-deploy-demo-2bede.appspot.com",
+  messagingSenderId: "333288959006",
+  appId: "1:333288959006:web:540fe3bcffeafdae56d20f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
